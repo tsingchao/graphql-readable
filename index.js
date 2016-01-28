a9b6b9fd957e0f1d6ed8000300ea4199acc2a9c8
tsingchao <qingchao.tan@gmail.com>
2016-01-28 15:08:23 +0800

    fix(index.js): remove the unuse package `fs` and `path`

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var invariant = require('graphql/jsutils/invariant')
-
 var buildClientSchema = require('graphql/utilities').buildClientSchema
 var printSchema = require('graphql/utilities').printSchema
 
 module.exports = function(schema) {
+  
   invariant(schema, 'Must provide schema')
   invariant(schema.data, 'Wrong schema')
 
