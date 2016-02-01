6032bfde8897061b0c733d3f9838d5080db242bd
tsingchao <qingchao.tan@gmail.com>
2016-02-01 11:58:37 +0800

    feat(index.js): add second argument `format` to support the type `GraphQL` or `JSON` to return

--- index.js
+++ index.js
@@ -1,16 +1,41 @@
-var invariant = require('graphql/jsutils/invariant')
-var buildClientSchema = require('graphql/utilities').buildClientSchema
-var printSchema = require('graphql/utilities').printSchema
+var invariant = require('graphql/jsutils/invariant');
+var buildClientSchema = require('graphql/utilities').buildClientSchema;
+var printSchema = require('graphql/utilities').printSchema;
 
-module.exports = function(schema) {
+module.exports = function(schema, format) {
 
-  invariant(schema, 'Must provide schema')
-  invariant(schema.data, 'Cannot read property \'data\' of schema')
+  invariant(schema, 'Must provide schema');
+  invariant(schema.data, 'Cannot read property `data` of schema');
+  invariant(format && typeof format === 'string', 'Please choose one of types which is `GraphQL` or `JSON`');
+
+  var clientSchema;
 
   try {
-    return printSchema(buildClientSchema(schema.data))
+    clientSchema = buildClientSchema(schema.data);
   } catch(error) {
-    throw new Error(error)
+    throw new Error(error);
   }
 
-}
+  switch(format.toLowerCase()) {
+    case 'graphql':
+      return printGraphQL(clientSchema);
+    case 'json':
+      return printJSON(clientSchema);
+    default:
+      throw new Error('Could only parse one of types which is `GraphQL` or `JSON`');
+      return '';
+  };
+
+};
+
+function printGraphQL(clientSchema) {
+
+  return printSchema(clientSchema);
+
+};
+
+function printJSON(clientSchema) {
+
+  return {};
+
+};
